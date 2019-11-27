fdbeb07e48bdbf8c5e05e43edd432025685caf3b
Brendon Kay <bak04280@gmail.com>
2019-11-26 20:46:36 -0700

    wip

--- set_1/challenge_6/challenge_6.go
+++ set_1/challenge_6/challenge_6.go
@@ -5,8 +5,14 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strings"
 )
+
+type Result struct {
+	Keysize              int
+	NormalHammingDistnce float64
+}
 
 func hammingDistance(a, b string) int {
 	// props to https://stackoverflow.com/a/40309527
@@ -30,9 +36,6 @@ func hammingDistance(a, b string) int {
 }
 
 // Set 1 Challenge 6 Cryptopals
-// TODO
-//	- Normalize k key length iteration over cipher text
-//	- ...
 func main() {
 	fmt.Println(hammingDistance("this is a test", "wokka wokka!!!"))
 
@@ -54,13 +57,36 @@ func main() {
 
 	fmt.Println(r)
 
-	// Find smallest Hamming distance from key sizes
-	keySizes := make([]int, 40)
-	for i := 1; i < len(keySizes); i++ {
+	// Find smallest average normalized Hamming distance from 4 key sizes
+	// TODO - keep in mind KEYSIZE was acquired taking the average of 2 distances.
+	var res []Result
+	for i := 2; i <= 40; i++ {
+		fmt.Println("keysize is:", i)
 		a := r[:i]
-		b := r[i : i+i]
-		keySizes[i] = hammingDistance(a, b)
+		fmt.Println("a is:", a)
+		b := r[i : 2*i]
+		fmt.Println("b is:", b)
+		c := r[2*i : 3*i]
+		fmt.Println("b is:", c)
+		d := r[3*i : 4*i]
+		fmt.Println("b is:", d)
+		x := float64(hammingDistance(a, b)) / float64(i)
+		y := float64(hammingDistance(c, d)) / float64(i)
+		res = append(res, Result{
+			Keysize:              i,
+			NormalHammingDistnce: (x + y) / 2,
+		})
 	}
 
-	fmt.Println(keySizes)
+	// Wicked awesome sorting closure.
+	sort.SliceStable(res, func(i, j int) bool {
+		return res[i].NormalHammingDistnce < res[j].NormalHammingDistnce
+	})
+	for _, s := range res {
+		fmt.Println(s.NormalHammingDistnce, s.Keysize)
+	}
+
+	// Take the keysize with the smallest average normalized hamming distance
+	k := res[0].Keysize
+	fmt.Println(k)
 }
